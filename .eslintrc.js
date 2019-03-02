1f76237ddabfa4fb5ffc274a9ed63db69e9e097c
kiriltaran <kiriltaran@gmail.com>
2019-03-02 10:46:54 +0200

    chore: add eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, {Component} from 'react';
-import './App.css';
-import ControlPanel from "./control-panel/ControlPanel";
-import FileZone from "./file-zone/FileZone";
-import getMockText from './text.service';
+import React, { Component } from 'react'
+import './App.css'
+import ControlPanel from './components/control-panel/ControlPanel'
+import FileZone from './components/file-zone/FileZone'
+import getMockText from './text.service'
 
 class App extends Component {
-    getText() {
-        getMockText().then(function (result) {
-            console.log(result);
-        });
-    }
-    render() {
-        return (
-            <div className="App">
-                <header>
-                    <span>Simple Text Editor</span>
-                </header>
-                <main>
-                    <ControlPanel/>
-                    <FileZone/>
-                </main>
-            </div>
-        );
-    }
+  state = {
+    text: '',
+  }
+  componentDidMount() {
+    this.getText()
+  }
+
+  async getText() {
+    const text = await getMockText()
+    this.setState({
+      text,
+    })
+  }
+  render() {
+    const { text } = this.state
+    return (
+      <div className="App">
+        <header>
+          <span>Simple Text Editor</span>
+        </header>
+        <main>
+          <ControlPanel />
+          <FileZone content={text} />
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
